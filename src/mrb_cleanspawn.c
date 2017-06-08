a1a6e42d10937bfa0e668b192c72a075b7623539
Uchio KONDO <udzura@pepabo.com>
2017-06-08 15:08:59 +0900

    Mark cloexec on fd >= 3

--- src/mrb_cleanspawn.c
+++ src/mrb_cleanspawn.c
@@ -11,7 +11,10 @@
 #include <mruby/error.h>
 #include <mruby/string.h>
 
+#include <dirent.h>
 #include <spawn.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -21,13 +24,16 @@
 
 extern char **environ;
 
-static mrb_value mrb_do_cleanspawn(mrb_state *mrb, mrb_value self) {
+static mrb_value mrb_do_cleanspawn(mrb_state *mrb, mrb_value self)
+{
   char *program;
   mrb_value *rest, ret;
   mrb_int argc;
   pid_t pid;
   char **argv;
   int i, status;
+  posix_spawn_file_actions_t file_actions;
+
   mrb_get_args(mrb, "z*", &program, &rest, &argc);
 
   argv = mrb_malloc(mrb, sizeof(char *) * (argc + 2));
@@ -38,7 +44,31 @@ static mrb_value mrb_do_cleanspawn(mrb_state *mrb, mrb_value self) {
   }
   argv[argc + 1] = NULL;
 
-  if (posix_spawn(&pid, program, NULL, NULL, argv, environ) != 0) {
+  if (posix_spawn_file_actions_init(&file_actions) != 0) {
+    mrb_sys_fail(mrb, "posix_spawn_file_actions_init");
+  }
+
+  int fd;
+  DIR *d = opendir("/proc/self/fd");
+  if (!d) {
+    mrb_sys_fail(mrb, "opendir: /proc/self/fd");
+  }
+
+  struct dirent *dp;
+  while ((dp = readdir(d)) != NULL) {
+    if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..")) {
+      /* Skip */
+    } else {
+      fd = (int)strtol(dp->d_name, NULL, 0);
+      if (fd > 2) {
+        if (posix_spawn_file_actions_addclose(&file_actions, fd) != 0) {
+          mrb_sys_fail(mrb, "posix_spawn_file_actions_addclose");
+        }
+      }
+    }
+  }
+
+  if (posix_spawn(&pid, program, &file_actions, NULL, argv, environ) != 0) {
     mrb_sys_fail(mrb, "posix_spawn");
   }
 
@@ -49,18 +79,24 @@ static mrb_value mrb_do_cleanspawn(mrb_state *mrb, mrb_value self) {
 
   if (WIFEXITED(status)) {
     ret = WEXITSTATUS(status) == 0 ? mrb_true_value() : mrb_false_value();
-  } else {
+  } else if (WIFSIGNALED(status)) {
+    mrb_warn(mrb, "may be signaled: %d", WTERMSIG(status));
     ret = mrb_false_value();
+  } else {
+    mrb_raisef(mrb, E_RUNTIME_ERROR, "Something is wrong. status: %S", mrb_fixnum_value(status));
   }
 
   return ret;
 }
 
-void mrb_mruby_clean_spawn_gem_init(mrb_state *mrb) {
+void mrb_mruby_clean_spawn_gem_init(mrb_state *mrb)
+{
   struct RClass *kern;
   kern = mrb->kernel_module;
   mrb_define_method(mrb, kern, "spawn", mrb_do_cleanspawn, MRB_ARGS_ANY());
   DONE;
 }
 
-void mrb_mruby_clean_spawn_gem_final(mrb_state *mrb) {}
+void mrb_mruby_clean_spawn_gem_final(mrb_state *mrb)
+{
+}
