9cc58055cab2dac5dd0f327c6085e5b33f3f8e73
Uchio KONDO <udzura@pepabo.com>
2017-06-08 15:53:05 +0900

    Add smoke test

--- test/mrb_cleanspawn.rb
+++ test/mrb_cleanspawn.rb
@@ -2,16 +2,12 @@
 ## CleanSpawn Test
 ##
 
-assert("CleanSpawn#hello") do
-  t = CleanSpawn.new "hello"
-  assert_equal("hello", t.hello)
+assert("CleanSpawn#clean_spawn") do
+  ret = clean_spawn "/bin/bash", "-c", "ls -l >/dev/null"
+  assert_true ret
 end
 
-assert("CleanSpawn#bye") do
-  t = CleanSpawn.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("CleanSpawn.hi") do
-  assert_equal("hi!!", CleanSpawn.hi)
+assert("CleanSpawn#clean_spawn when failed") do
+  ret = clean_spawn "/bin/bash", "-c", "nonexist 2>/dev/null"
+  assert_false ret
 end
